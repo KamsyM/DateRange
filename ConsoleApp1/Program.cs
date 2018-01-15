c9daf9b2d26150dc9586eae26f1f0dcf983cdba2
Kamsi Mbanefo <Kamsimbanefo@yahoo.com>
2018-01-15 10:51:07 +0000

    DateRange Class Work

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var michaelMassTerm = new DateRange(new DateTime(2016, 9, 8), 1);
+            Console.WriteLine(michaelMassTerm);
+            DateTime a = new DateTime(2016, 6, 7);
+            Console.WriteLine(michaelMassTerm.Contains(a));
+            Console.ReadKey();
+        }
+    }
+    public class DateRange
+    {
+        private DateTime FromDate;
+        private DateTime ToDate;
+
+        public DateRange(DateTime from, DateTime to)
+        {
+            FromDate = from;
+            ToDate = to;
+        }
+
+        public DateRange( DateTime from, double no)
+        {
+            FromDate = from;
+            ToDate = from.AddDays(no);
+
+        }
+
+
+        public override string ToString()
+        {
+            return FromDate.ToString("d") + " to " + ToDate.ToString("d");
+        }
+
+        public bool Contains(DateTime date)
+        {
+            return date >= FromDate && date <= ToDate;
+        }
+
+}
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var michaelMassTerm = new DateRange(new DateTime(2016, 9, 8), 1);
+            DateTime a = new DateTime(2016, 6, 7);
+            Assert.IsFalse(michaelMassTerm.Contains(a));
+        }
+    }
+}
